c1f7a7bb23dcd365c8e65453d1947dbd314cdd3c
Russell Currey <ruscur@russell.cc>
2016-09-29 08:57:55 +1000

    Part 4 done
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/bin/part4.rs
+++ src/bin/part4.rs
@@ -1,6 +1,4 @@
-// Part 4: structs
-
-// Use structs to make our program more awesome.
+// Part 4: structs, borrowing and mutability
 
 extern crate rand;
 
@@ -24,12 +22,12 @@ impl Room {
         }
     }
 
-    fn add_star(&self, new_star: Point) {
+    fn add_star(&mut self, new_star: Point) {
         if new_star.x == 0 || new_star.x == self.length-1 ||
             new_star.y == 0 || new_star.y == self.width-1 {
             return;
         }
-        for star in self.stars {
+        for star in &self.stars {
             if new_star.x == star.x && new_star.y == star.y {
                 return
             }
@@ -41,7 +39,7 @@ impl Room {
     // randomly decide where to place the star
     // stars can only be drawn inside the room, which has an area of
     // length-2, width-2, then push it out of wall range with +1
-    fn add_random_star(&self) {
+    fn add_random_star(&mut self) {
         let new_star = Point {
             x: (rand::random::<u8>() % (self.length-2)) + 1,
             y: (rand::random::<u8>() % (self.width-2)) + 1
@@ -49,9 +47,20 @@ impl Room {
 
         self.stars.push(new_star);
     }
+
+    fn is_star_at_point(&self, current_point: &Point) -> bool {
+        for star in &self.stars {
+            if star.x == current_point.x && star.y == current_point.y {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
-fn point_is_wall(room: Room, point: Point) -> bool {
+// part4: change Room to &Room and explain moved values and lifetimes
+fn point_is_wall(room: &Room, point: &Point) -> bool {
     point.y == 0 || point.y == room.width-1 || point.x == 0 || point.x == room.length-1
 
     // mention this ^ is the same as if that { return true } else { return false }
@@ -62,7 +71,7 @@ fn main() {
 
     let star = "🌟";
 
-    let room = Room::new(20, 8);
+    let mut room = Room::new(20, 8);
     room.add_random_star();
 
     // for each line (down)
@@ -70,8 +79,11 @@ fn main() {
         // for each character (across)
         for x in 0..room.length {
             // if we are drawing a wall
-            if point_is_wall(room, Point{x: x, y: y}) {
+            let current_point = Point{x: x, y: y};
+            if point_is_wall(&room, &current_point) {
                 print!("=");
+            } else if room.is_star_at_point(&current_point) {
+                print!("{}", star);
             } else { // if we aren't drawing a wall
                 print!(" ");
             }
