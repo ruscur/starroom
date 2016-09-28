ddcea0da28e62962f9f69314a7ceb20dbe479a14
Russell Currey <ruscur@russell.cc>
2016-09-28 15:47:47 +1000

    Doesnt compile yet about to board LUL
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/bin/part4.rs
+++ src/bin/part4.rs
@@ -12,16 +12,42 @@ struct Point {
 struct Room {
     length: u8,
     width: u8,
-    stars: mut Vec<Point>
+    stars: Vec<Point>
 }
 
 impl Room {
-    fn new(length: u8, width: u8) {
+    fn new(length: u8, width: u8) -> Room {
         Room {
             length: length,
             width: width,
             stars: vec!()
         }
+    }
+
+    fn add_star(&self, new_star: Point) {
+        if new_star.x == 0 || new_star.x == self.length-1 ||
+            new_star.y == 0 || new_star.y == self.width-1 {
+            return;
+        }
+        for star in self.stars {
+            if new_star.x == star.x && new_star.y == star.y {
+                return
+            }
+        }
+
+        self.stars.push(new_star);
+    }
+
+    // randomly decide where to place the star
+    // stars can only be drawn inside the room, which has an area of
+    // length-2, width-2, then push it out of wall range with +1
+    fn add_random_star(&self) {
+        let new_star = Point {
+            x: (rand::random::<u8>() % (self.length-2)) + 1,
+            y: (rand::random::<u8>() % (self.width-2)) + 1
+        };
+
+        self.stars.push(new_star);
     }
 }
 
@@ -35,27 +61,17 @@ fn main() {
     println!("Welcome to the star room");
 
     let star = "🌟";
-    let room = Room {
-        length = 20,
-        width = 8,
-        stars = 1
-    };
 
-    // randomly decide where to place the star
-    // stars can only be drawn inside the room, which has an area of
-    // length-2, width-2, then push it out of wall range with +1
-    let star_x = (rand::random::<u8>() % (length-2)) + 1;
-    let star_y = (rand::random::<u8>() % (width-2)) + 1;
+    let room = Room::new(20, 8);
+    room.add_random_star();
 
     // for each line (down)
-    for y in 0..width {
+    for y in 0..room.width {
         // for each character (across)
-        for x in 0..length {
+        for x in 0..room.length {
             // if we are drawing a wall
-            if point_is_wall(length, width, x, y) {
+            if point_is_wall(room, Point{x: x, y: y}) {
                 print!("=");
-            } else if x == star_x && y == star_y {
-                print!("{}", star);
             } else { // if we aren't drawing a wall
                 print!(" ");
             }
