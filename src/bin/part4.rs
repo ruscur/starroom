9bebaafc27a5a003a9a881d5cd2f7dec5c05d8c0
Russell Currey <ruscur@russell.cc>
2016-09-30 16:32:53 +1000

    part4: put everything into the impl (too much for one part??)
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- src/bin/part4.rs
+++ src/bin/part4.rs
@@ -22,6 +22,14 @@ impl Room {
         }
     }
 
+    fn new_with_stars(length: u8, width: u8, stars: Vec<Point>) -> Room {
+        Room {
+            length: length,
+            width: width,
+            stars: stars
+        }
+    }
+
     fn add_star(&mut self, new_star: Point) {
         if new_star.x == 0 || new_star.x == self.length-1 ||
             new_star.y == 0 || new_star.y == self.width-1 {
@@ -57,37 +65,39 @@ impl Room {
 
         return false;
     }
-}
 
-// part4: change Room to &Room and explain moved values and lifetimes
-fn point_is_wall(room: &Room, point: &Point) -> bool {
-    point.y == 0 || point.y == room.width-1 || point.x == 0 || point.x == room.length-1
+    fn point_is_wall(&self, point: &Point) -> bool {
+        point.y == 0 || point.y == self.width-1
+            || point.x == 0 || point.x == self.length-1
+    }
 
-    // mention this ^ is the same as if that { return true } else { return false }
+    fn draw(&self) {
+        let star = "🌟";
+
+        // for each line (down)
+        for y in 0..self.width {
+            // for each character (across)
+            for x in 0..self.length {
+                // if we are drawing a wall
+                let current_point = Point{x: x, y: y};
+                if self.point_is_wall(&current_point) {
+                    print!("=");
+                } else if self.is_star_at_point(&current_point) {
+                    print!("{}", star);
+                } else { // if we aren't drawing a wall
+                    print!(" ");
+                }
+            }
+            print!("\n");
+        }
+    }
 }
 
 fn main() {
     println!("Welcome to the star room");
 
-    let star = "🌟";
-
     let mut room = Room::new(20, 8);
     room.add_random_star();
 
-    // for each line (down)
-    for y in 0..room.width {
-        // for each character (across)
-        for x in 0..room.length {
-            // if we are drawing a wall
-            let current_point = Point{x: x, y: y};
-            if point_is_wall(&room, &current_point) {
-                print!("=");
-            } else if room.is_star_at_point(&current_point) {
-                print!("{}", star);
-            } else { // if we aren't drawing a wall
-                print!(" ");
-            }
-        }
-        print!("\n");
-    }
+    room.draw();
 }
